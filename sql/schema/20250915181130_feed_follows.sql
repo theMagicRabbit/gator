572de7a02982e6f98bf42c9d6e0d04f93fd87490
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-09-15 13:24:08 -0500

    Add feed_follows table

---
+++ sql/schema/20250915181130_feed_follows.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE feed_follows (
+    id uuid UNIQUE NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_ad timestamp NOT NULL,
+    user_id uuid NOT NULL,
+    feed_id uuid NOT NULL,
+    CONSTRAINT fk_feed_follows_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_feed_follows_feed_id FOREIGN KEY (feed_id) REFERENCES feeds(id) ON DELETE CASCADE,
+    CONSTRAINT uq_user_id_feed_id UNIQUE (user_id, feed_id)
+);
+
+-- +goose Down
+DROP TABLE feed_follows;
