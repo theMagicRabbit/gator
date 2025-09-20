a07b894c95b8cb57e8d0fdaf6135a821c63badf0
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-09-20 15:37:14 -0500

    Add post queries

--- sql/schema/20250920172024_posts.sql
+++ sql/schema/20250920172024_posts.sql
@@ -9,7 +9,7 @@ CREATE TABLE posts (
     published_at timestamp,
     feed_id uuid NOT NULL,
     CONSTRAINT pk_posts PRIMARY KEY (id),
-    CONSTRAINT fk_posts_feed_id FOREIGN KEY (feed_id) REFERENCES feeds.id ON DELETE CASCADE,
+    CONSTRAINT fk_posts_feed_id FOREIGN KEY (feed_id) REFERENCES feeds (id) ON DELETE CASCADE,
     CONSTRAINT ck_posts_title_description_or_null CHECK (description IS NOT NULL OR title IS NOT NULL)
 );
 
