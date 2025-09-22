f9673f8317c21dffd2a3ba2d98e167bd10fa308b
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-09-22 10:18:38 -0500

    Sort posts DESC
    derp

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -7,6 +7,6 @@ RETURNING *;
 SELECT posts.* FROM posts
 JOIN feed_follows ON feed_follows.feed_id = posts.feed_id
 WHERE feed_follows.user_id = $1
-ORDER BY posts.published_at ASC NULLS LAST
+ORDER BY posts.published_at DESC NULLS LAST
 LIMIT $2;
 
